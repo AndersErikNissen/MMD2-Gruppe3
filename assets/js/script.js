05e037931ce0be1e7a659db0a65471a795467624
Anders <1086088@ucn.dk>
2021-05-31 18:40:55 +0200

    XML v1 - virker + start på makeSite

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,63 @@
 //JS by Gruppe 3 (Anders, Anna, Caroline & Nadia)
 //UCN MMDA0920
 
- 
+
+const   login = {
+            "username":"api.user",
+            "password":"API-key-1234#!"
+        },
+        URLsite = "https://aenders.dk/wp-json/wp/v2/",
+        URLtoken = "https://aenders.dk/wp-json/jwt-auth/v1/token";
+
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () { // Onreadystatechange, bliver aktiveret når f.eks hjemmesiden bliver åbnet og skal til at loade. 
+    if (this.readyState == 4 && this.status == 200) { // readyState == 4, betyder at den er færdig og har gennemført det som blev spurgt efter, og status == 200 betyder at den metode som er blevet spurgt efter har lykkedes.
+        try {
+        let REtoken = JSON.parse(this.response); //JSON laver den data vi har fået fra vores API om til et JSON-format så vi kan arbejde med det i JaveScript.
+        window.localStorage.setItem("token", REtoken.token); //Gemmer den token vi får tilbage på i LocalStorage i browseren.
+        afterToken(); // Functionen som skal anvende denne token til at spørge API om data.
+        } catch(error) {
+            console.log("Parsing Error: " + error)
+        }
+    }
+    if (this.readyState == 4 && this.status >= 400) { // Altså at hvis der har være kontakt med API'en men den kunne ikke få et ordenligt svar tilbage, så skal en besked vises.
+        console.log("Der opstod en fejl, være venlig at vende tilbage senere!")
+    }
+}
+xhttp.open("POST", URLtoken, true) // POST fordi vi skal give noget til API'en + få noget tilbage.
+xhttp.setRequestHeader("Content-Type", "application/JSON"); 
+xhttp.send(JSON.stringify(login)); //JSON.stringify er det modsatte af JSON.parse, i dette tilfælde laver den JSON data om til en string som API'en kan forstå. 
+
+function afterToken() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () { // Onreadystatechange, bliver aktiveret når f.eks hjemmesiden bliver åbnet og skal til at loade. 
+    if (this.readyState == 4 && this.status == 200) { // readyState == 4, betyder at den er færdig og har gennemført det som blev spurgt efter, og status == 200 betyder at den metode som er blevet spurgt efter har lykkedes.
+        try {
+        const data = JSON.parse(this.response);
+        makeSite(data);
+        } catch(error) {
+            console.log("Parsing Error: " + error)
+        }
+    }
+    if (this.readyState == 4 && this.status >= 400) { 
+        console.log("Der opstod en fejl, være venlig at vende tilbage senere!")
+    }
+}
+xhttp.open("GET", URLsite + "posts?status=private&categories=17&per_page=100", true) // GET fordi nu hvor vi har vores token, så vil vi gerne spørge om noget data vi kan arbejde med. 
+xhttp.setRequestHeader("Authorization", "Bearer " + window.localStorage.getItem("token")); //For at kunne få data, skal bruge vores login, som her er en token som sendes sammed med vores GET-request.
+xhttp.send();
+}
+
+function makeSite(data) {
+    console.log(data[0])
+
+    function makeTitle (site) {
+        let split = site.title.rendered.split("Private: "),
+            title = split[1];
+        
+        document.title = title + " - SNV.dk";
+    }
+    
+    function getURL (URL)
+    //Lave matchID og FindpageID sammen med drawSite??
+}
